2c0e7581836f2d43843a00de929fcf269418fe1b
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-03-17 21:00:53 -0300

    statefull and stateless

---
+++ lib/home_page_statefull.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePageStatefull extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageStateFullState();
+  }
+}
+
+class _HomePageStateFullState extends State<HomePageStatefull> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
