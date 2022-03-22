1e7184738a4472d2fd3e14432790faa99f2475fd
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-03-22 08:17:28 -0300

    media query page

---
+++ lib/pages/media_query/media_query_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MediaQuery extends StatelessWidget {
+  const MediaQuery({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
