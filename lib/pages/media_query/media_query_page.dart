e7d8ca46413e421aa6616c9182837058ef6ad299
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-03-22 08:37:41 -0300

    mediaquery

--- lib/pages/media_query/media_query_page.dart
+++ lib/pages/media_query/media_query_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
-class MediaQuery extends StatelessWidget {
-  const MediaQuery({ Key? key }) : super(key: key);
+class MediaQueryPage extends StatelessWidget {
+  const MediaQueryPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final mediaQuery = MediaQuery.of(context);
+    final statusBar = mediaQuery.padding.top;
+    final heightBody = mediaQuery.size.height - statusBar;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MediaQuery'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              width: mediaQuery.size.width * .5,
+              height: heightBody * .5,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
